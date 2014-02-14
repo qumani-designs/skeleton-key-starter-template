15cecba621e9d701fdf2b58fe83e84b9117d1e2e
justin <balesteri.j@gmail.com>
2014-02-14 14:32:45 -0700

    add footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+	<footer class="max-width centered">
+		<div>
+			<div>
+				<div class="footer-block">
+					<ul class="bulleted">
+						<li>
+							<a href="#">example</a>
+						</li>
+						<li>
+							<a href="#">another</a>
+						</li>
+						<li>
+							<a href="#">one more</a>
+						</li>
+						<li>
+							<a href="#">one last one</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
