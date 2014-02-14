3b120fcd52963e9fc3bcc83154307df780d16804
Justin Balesteri <balesteri.j@gmail.com>
2014-02-14 08:20:02 -0800

    initial commit

---
+++ skeleton-key.php
@@ -0,0 +1,42 @@
+/**
+ * @name       Skeleton Key Project Template
+ * @copyright  Copyright (c) 2014 Justin Balesteri. DBA Qumani Designs (http://qumani.com)
+ * @license    licesnsed under The MIT License (MIT) http://opensource.org/licenses/MIT
+ */ 
+
+<html class="no-js" lang="en" >
+	<!--<![endif]-->
+	<head>
+		<?php include 'head.php'; ?>
+	</head>
+	<body>
+			<div class="wrapper">
+			
+				<!-- include any off-canvas or responsive navigation goes -->
+				
+				<section class="main-section">
+					<?php include 'header.php'; ?>
+					 <div class="hero-area">
+						<!-- include large header here -->
+					</div>
+					<!-- content begin -->
+					<div class="content-wrapper">
+						<section class="page-content">
+							<h1>Title</h1>
+							<!-- content goes here -->
+						</section>
+					</div>
+				</section><!-- main-content end -->
+				<!-- content end -->
+			</div><!-- inner-wrap end -->
+		<div class="footer-outer">
+			<?php include 'footer.php'; ?>
+		</div><!-- footer-outer end -->
+
+		<script src="js/jquery.js"></script>
+		<!-- other libraries -->
+		
+				<!-- Dont forget your to edit analytics php file -->
+				<?php include 'header.php'; ?>
+	</body>
+</html>
