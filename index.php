d7b80609bb45cd0ca963a1abf1e5ff852bdc8083
justin <balesteri.j@gmail.com>
2014-02-14 14:31:26 -0700

    add index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!--
+/**
+* @name       Skeleton Key Project Template
+* @copyright  Copyright (c) 2014 Justin Balesteri. DBA Qumani Designs (http://qumani.com)
+* @license    licesnsed under The MIT License (MIT) http://opensource.org/licenses/MIT
+*/
+-->
+
+<html class="no-js" lang="en" >
+	<!--<![endif]-->
+	<head>
+		<?php
+		include 'head.php';
+		?>
+	</head>
+	<body>
+		<div class="wrapper">
+
+			<!-- include any off-canvas or responsive navigation goes -->
+
+			<section class="main-section">
+				<?php
+				include 'header.php';
+				?>
+				<!-- if you dont want a banner image then just comment out or remove hero-holder and contents-->
+				<div class="hero-holder">
+					<div class="hero-area min-height max-width centered">
+						<img src="http://placehold.it/1000x400&text=Banner+Image" />
+					</div>
+				</div>
+				<!-- content begin -->
+				<div class="content-wrapper">
+					<section class="page-content min-height max-width centered">
+						<h1>h1 title</h1>
+						<!-- content goes here -->
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<hr />
+						<h2>h2 sub-title</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<hr />
+						<h3>h3 sub-title</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<hr />
+						<h4>h4 sub-title</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<hr />
+						<h5>h5 sub-title</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<hr />
+						<h6>h6 sub-title</h6>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<!-- content goes above -->
+					</section>
+				</div>
+			</section><!-- main-content end -->
+			<!-- content end -->
+		</div><!-- inner-wrap end -->
+		<div class="footer-outer">
+			<?php
+			include 'footer.php';
+			?>
+			<div class="copyright">
+				<p>&copy; 2014 <a href="http://qumani.com">Qumani Designs</a></p>
+			</div>
+		</div><!-- footer-outer end -->
+
+		<!-- <script src="js/jquery.js"></script> -->
+		<!-- other libraries -->
+
+		<!-- Dont forget your to edit analytics php file -->
+		
+	</body>
+</html>
