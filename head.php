b23f47ad4a6ca0c07087b652b7c69802c9585952
justin <balesteri.j@gmail.com>
2014-02-14 10:08:04 -0700

    Add head.php

---
+++ head.php
@@ -0,0 +1,6 @@
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<title>Qumani Designs | Web &amp; Graphic Design</title>
+<link href='css/style.css' rel='stylesheet' />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
