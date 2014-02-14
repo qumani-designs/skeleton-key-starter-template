fa9c179c583be5c6d7a0ebe6170233a1422089bf
justin <balesteri.j@gmail.com>
2014-02-14 11:42:13 -0700

    add header.php

---
+++ header.php
@@ -0,0 +1,35 @@
+<header>
+	<div class="header-inner">
+		<div id="logo left">
+			<a href="index.php">
+				<img src="http://placehold.it/200x100" alt="logo">
+			</a>
+		</div>
+		<!-- uncomment below for use of an off canvas navigation -->
+		<!--
+		<nav class="off-canvas-nav">
+			<section>
+				<a class="#" ><span></span></a>
+			</section>
+		</nav>
+		-->
+		<!-- uncomment above for use of an off canvas navigation -->
+		<div class="nav-holder right">
+			<nav class="top-nav">
+				<section>
+					<ul class="inline">
+						<li>
+							<a href="index.php">example</a>
+						</li>
+						<li>
+							<a href="contact.php">example</a>
+						</li>
+					</ul>
+				</section>
+			</nav>
+		</div><!-- navHolder -->
+	</div>
+</header>
+
+		
+
